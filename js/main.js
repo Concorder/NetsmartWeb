86cdf598a6110999b9fe8ef5be72a51baef1a91f
Concorder <56825826+Concorder@users.noreply.github.com>
2023-09-30 16:44:16 +0300

    blocks rearrange, hero, text

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-setCurrentYear = function (){
+setCurrentYear = function () {
   const today = new Date();
   let fullYear = today.getFullYear()
   const yearSpan = document.getElementById("year");
@@ -9,12 +9,12 @@ setCurrentYear()
 
 //gsap block appearance
 const blocksUp = document.querySelectorAll(".gsapUp")
-blocksUp.forEach(block=>{
+blocksUp.forEach(block => {
   gsap.from(block, {
     scrollTrigger: {
       trigger: block,
       start: "top bottom",
-      end: "top center",
+      end: "+=300px",
       scrub: 0.5,
       markers: false,
       toggleActions: "restart none reverse none",
@@ -31,12 +31,12 @@ blocksUp.forEach(block=>{
   });
 });
 const blocksLeft = document.querySelectorAll(".gsapLeft")
-blocksLeft.forEach(block=>{
+blocksLeft.forEach(block => {
   gsap.from(block, {
     scrollTrigger: {
       trigger: block,
       start: "top bottom",
-      end: "top center",
+      end: "+=300px",
       scrub: 1,
       markers: false,
       toggleActions: "restart none reverse none",
@@ -54,12 +54,12 @@ blocksLeft.forEach(block=>{
   });
 })
 const blocksRight = document.querySelectorAll(".gsapRight")
-blocksRight.forEach(block=>{
+blocksRight.forEach(block => {
   gsap.from(block, {
     scrollTrigger: {
       trigger: block,
       start: "top bottom",
-      end: "top center",
+      end: "+=300px",
       scrub: 1,
       markers: false,
       toggleActions: "restart none reverse none",
@@ -98,7 +98,26 @@ sentences.forEach(function (target) {
     paused: false,
   });
 });
-
+//on screen trigger
+const blocksOnScreen = document.querySelectorAll(".onScreenTrigger")
+blocksOnScreen.forEach(function (target) {
+  gsap.to(target, {
+    scrollTrigger: {
+      trigger: target,
+      start: "top bottom",
+      end: "bottom top",
+      scrub: 1,
+      markers: false,
+      toggleClass: "onScreen",
+    },
+    delay: .3,
+    repeat: 0,
+    repeatDelay: 0,
+    ease: "power0",
+    duration: 1,
+    paused: false,
+  });
+});
 
 // splashtext
 function splashText() {
@@ -115,7 +134,6 @@ function splashText() {
     }
     word.innerHTML = newWord;
   });
-  //move prize to the new line
   let lamps = document.querySelectorAll('.cta i');
   lamps.forEach(function (e) {
     var randomPoseX = (Math.ceil(Math.random() * 1000));
@@ -127,7 +145,77 @@ function splashText() {
 }
 
 splashText()
-//3d phone
+
+//hacker text
+const alphabet = [
+  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+  "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+];
+
+let letter_count = 0;
+const el = document.getElementById("loading");
+const word = el.innerHTML.trim();
+let finished = false;
+
+el.innerHTML = "";
+
+for (let i = 0; i < word.length; i++) {
+  const span = document.createElement("span");
+  span.innerHTML = word.charAt(i);
+  el.appendChild(span);
+}
+const spans = document.querySelectorAll("#loading span");
+
+
+ScrollTrigger.create({
+  trigger: '.onScreenTrigger',
+  onEnter: hackerText,
+
+})
+
+function hackerText() {
+
+  setTimeout(write, 75);
+  const incrementer = setTimeout(inc, 1000);
+  console.log("func")
+  reset()
+}
+
+
+function write() {
+  if (el.classList.contains('onScreen')) {
+    for (let i = letter_count; i < word.length; i++) {
+      const c = Math.floor(Math.random() * 36);
+      spans[i].innerHTML = alphabet[c];
+      console.log("write")
+    }
+    if (!finished) {
+      setTimeout(write, 75);
+
+    }
+  }
+}
+
+function inc() {
+  if (el.classList.contains('onScreen')) {
+  spans[letter_count].innerHTML = word[letter_count];
+  spans[letter_count].classList.add("glow");
+  letter_count++;
+  if (letter_count >= word.length) {
+    finished = true;
+  } else {
+    setTimeout(inc, 200);
+  }
+}}
+
+function reset() {
+  letter_count = 0;
+  finished = false;
+  spans.forEach(span => span.classList.remove("glow"));
+}
+
+
 
 
 
