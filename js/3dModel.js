08e34fbce80b594a909c75b1deb6033fd037ea31
Concorder <56825826+Concorder@users.noreply.github.com>
2024-01-08 12:19:11 +0200

    optimization

--- js/3dModel.js
+++ js/3dModel.js
@@ -1,96 +1,81 @@
+import * as THREE from 'https://cdn.skypack.dev/pin/three@v0.136.0-4Px7Kx1INqCFBN0tXUQc/mode=imports,min/optimized/three.js'
+import {GLTFLoader} from "https://cdn.skypack.dev/pin/three@v0.136.0-4Px7Kx1INqCFBN0tXUQc/mode=imports,min/unoptimized/examples/jsm/loaders/GLTFLoader.js";
+
 let vh = window.innerHeight * 0.01;
 let vw = window.innerWidth;
 const heroSection = document.querySelector('.hero');
 
 window.addEventListener('resize', () => {
-  vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
-if (window.innerWidth > 492) {
-  import('https://cdn.skypack.dev/three@0.136').then((module) => {
-    const THREE = module;
-    import('https://cdn.skypack.dev/three@0.136/examples/jsm/controls/OrbitControls.js').then((OrbitControls) => {
-      return import('https://cdn.skypack.dev/three@0.136/examples/jsm/loaders/RGBELoader.js').then((module) => {
-        const { RGBELoader } = module;
-        import('https://cdn.skypack.dev/three@0.136/examples/jsm/loaders/GLTFLoader.js').then((GLTFLoader) => {
-          let container;
-          let camera;
-          let renderer;
-          let scene;
-          let model;
 
-          function init() {
-            let rotationSpeed = 0.001;
-            container = document.getElementById('threeModel');
-            // Create scene
-            scene = new THREE.Scene();
+let container;
+let camera;
+let renderer;
+let scene;
+let model;
 
-            const fov = 35;
-            const aspect = container.offsetWidth / container.offsetHeight;
-            const near = 0.7;
-            const far = 100;
+function init() {
+    let rotationSpeed = 0.001;
+    container = document.getElementById('threeModel');
+    // Create scene
+    scene = new THREE.Scene();
 
-            // Camera setup
-            camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-            camera.position.set(0, 0,15);
+    const fov = 35;
+    const aspect = container.offsetWidth / container.offsetHeight;
+    const near = 1;
+    const far = 100;
 
-            const ambient = new THREE.AmbientLight(0x404040, 2);
-            scene.add(ambient);
+    // Camera setup
+    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 1, 5);
 
-            const light = new THREE.DirectionalLight(0xffffff, 1);
-            light.position.set(-20, 20,20);
-            scene.add(light);
+    const ambient = new THREE.AmbientLight(0x404040, 0.2);
+    scene.add(ambient);
 
-            // Renderer
-            renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-            renderer.setSize(container.clientWidth, container.clientHeight);
-            renderer.setPixelRatio(window.devicePixelRatio);
-            renderer.toneMappingExposure = 2;
-            renderer.outputEncoding = THREE.sRGBEncoding;
-            container.appendChild(renderer.domElement);
+    const light = new THREE.DirectionalLight(0xffffff, 2);
+    light.position.set(-20, 20, 20);
+    scene.add(light);
 
-            // Load Model
-            let loader = new GLTFLoader.GLTFLoader();
-            loader.load('./3d/planet_earth/scene.gltf', function (gltf) {
-              model = gltf.scene;
-              scene.add(model);
-              model.rotation.x = 0.2;
-              model.rotation.y = 1;
-              animate();
-            });
+    // Renderer
+    renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.toneMappingExposure = 1;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    container.appendChild(renderer.domElement);
 
-
-            window.addEventListener('dblclick', () => {
-              console.log(camera.position);
-            });
-
-
-            function animate() {
-              requestAnimationFrame(animate);
-
-              // if (model.rotation.y >= 3.8) {
-              //   rotationSpeed = -0.001;
-              // } else {
-              //   if (model.rotation.y <= 2.7) rotationSpeed = 0.001;
-              // }
-              model.rotation.y += rotationSpeed;
-container.classList.add("visible")
-              renderer.render(scene, camera);
-            }
-          }
-
-          init();
-
-          function onWindowResize() {
-            camera.aspect = container.clientWidth / container.clientHeight;
-            camera.updateProjectionMatrix();
-            renderer.setSize(container.clientWidth, container.clientHeight);
-          }
-
-          window.addEventListener('resize', onWindowResize);
-        });
-      });
+    // Load Model
+    let loader = new GLTFLoader();
+    loader.load('./3d/earth/scene.gltf', function (gltf) {
+        model = gltf.scene;
+        scene.add(model);
+        model.rotation.x = 0.2;
+        model.rotation.y = 1;
+        animate();
     });
-  });
+
+    window.addEventListener('dblclick', () => {
+        console.log(camera.position);
+    });
+
+    function animate() {
+        requestAnimationFrame(animate);
+        model.rotation.y += rotationSpeed;
+        container.classList.add('visible');
+        renderer.render(scene, camera);
+    }
 }
+
+init();
+
+function onWindowResize() {
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+window.addEventListener('resize', onWindowResize);
+
